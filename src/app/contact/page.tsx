8e8a628039eae5d71bb95fb12f610ff8a5f0827c
ahmadawji <ahmadawjics@gmail.com>
2023-08-08 22:07:20 +0300

    Add next navigation

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,20 @@
-export default function Contact(){
-    return <div>Contact</div>
+"use client";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+export default function Contact() {
+  const router = useRouter();
+  return (
+    <div>
+      <h1>Contact</h1>
+      <Link href={"/"}>home</Link>
+      <button
+        onClick={() => {
+          router.push("/");
+        }}
+      >
+        home
+      </button>
+    </div>
+  );
 }
